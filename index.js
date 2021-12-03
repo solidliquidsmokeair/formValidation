b9e457a2b565fe5052eb440eeafe51d5a3c5b342
solidliquidsmokeair <sjgogan@icloud.com>
2021-12-04 12:16:17 +1300

    build form and initial variables

---
+++ index.js
@@ -0,0 +1,20 @@
+let accName = document.querySelector('.nameField')
+let accEmail = document.querySelector('.emailField')
+let accEmail = document.querySelector('.usernameField')
+let accEmail = document.querySelector('.passwordField')
+
+let submit = document.getElementById('submit')
+
+var users = [
+    {
+        id: 1,
+        name: name,
+        email: email,
+        username: username,
+        password: password,
+    }
+]
+
+submit.addEventListener("click", function(){
+    console.log('hello world')
+})
