03d00e00710d7486106c7cd642f7532a5cd1a8ef
solidliquidsmokeair <sjgogan@icloud.com>
2021-12-04 12:20:20 +1300

    build validate form Function

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 let accName = document.querySelector('.nameField')
 let accEmail = document.querySelector('.emailField')
-let accEmail = document.querySelector('.usernameField')
-let accEmail = document.querySelector('.passwordField')
+let accUsername = document.querySelector('.usernameField')
+let accPassword = document.querySelector('.passwordField')
 
 let submit = document.getElementById('submit')
 
 var users = [
     {
         id: 1,
-        name: name,
-        email: email,
-        username: username,
-        password: password,
+        name: 'name',
+        email: 'email',
+        username: 'username',
+        password: 'password',
     }
 ]
 
+function validateInput (text){
+    console.log(text)
+}
+
 submit.addEventListener("click", function(){
-    console.log('hello world')
+   validateInput(accEmail)
 })
