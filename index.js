db9d05813898310ad68b02a24ea211cd28615921
solidliquidsmokeair <sjgogan@icloud.com>
2021-12-05 23:04:46 +1300

    more css

--- index.js
+++ index.js
@@ -7,36 +7,18 @@ let nameErr = document.getElementsByClassName('.nameErr')
 
 let submit = document.getElementById('submit')
 
-var users = [
-    {
-        id: 1,
-        name: 'name',
-        email: 'email',
-        username: 'username',
-        password: 'password',
-    }
-]
+let form = document.getElementById('form')
 
-function validateInput (){
-    if(accName.value.trim()==''){
-        let messageEle=parent.querySelector('.nameErr');
-        messageEle.style.visibility="visible";
-        messageEle.innerText="user name cannot be empty"
-    }  else if(accEmail.value.trim()==''){
-        console.log('no email')
-    } else if(accUsername.value.trim()==''){
-        console.log('no user')
-    } else if(accPassword.value.trim()==''){
-        console.log('no pass')
-    } else{
-        users[0].name = accName.value
-        users[0].email = accEmail.value
-        users[0].username = accUsername.value
-        users[0].password = accPassword.value
-    }
-}
+document.addEventListener('submit', () => {
+    
+    let errors = []
 
-submit.addEventListener("click", function(event){
-    event.preventDefault()
-   validateInput()
+    if(accName.innerText === '' || accName.innerText == null){
+        errors.push('name field cannot be empty')
+        nameErr.innerText = errors
+    }
+
+    if(errors.length > 0){
+        console.log('no errors')
+    }
 })
