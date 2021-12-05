f2587f58e664ca437645d16113615e13159b646c
solidliquidsmokeair <sjgogan@icloud.com>
2021-12-06 02:50:40 +1300

    clean up code

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ let accEmail = document.querySelector('.emailField')
 let accUsername = document.querySelector('.usernameField')
 let accPassword = document.querySelector('.passwordField')
 
-let signInName = document.querySelector('.signInUsernameField')
-let signInPassword = document.querySelector('.signInPasswordField')
-
 let signUp = document.getElementById('signUpButton')
 let signIn = document.getElementById('signInButton')
 
 let form = document.getElementById('form')
+
+let nameErr = document.querySelector('.nameErr')
+let passErr = document.querySelector('.passErr')
+
 
 var users = [
     {
@@ -30,9 +31,6 @@ function submitForm(){
     const user = document.querySelector('.signInUsernameField').value
     const pass = document.querySelector('.signInPasswordField').value
 
-    let nameErr = document.querySelector('.nameErr')
-    let passErr = document.querySelector('.passErr')
-
     if(user === users[0].username && pass === users[0].password){
         window.location.replace("./account.html");
     }else{
@@ -48,12 +46,6 @@ function submitForm(){
         passErr.innerText = 'invalid password'
         console.log('wrong')
     }
-
-    // if(user.innerText === users[0].username && signInPassword.innerText === users[0].password){
-    //     console.log('hooray')
-    // }
-
-// console.log('hello')
 }
 
 signIn.addEventListener('click', function(){
