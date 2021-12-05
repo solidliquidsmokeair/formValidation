ad9ec54ed15fb002ef35b44f610ba1ad564e4262
solidliquidsmokeair <sjgogan@icloud.com>
2021-12-06 02:46:12 +1300

    sign in validation function written

--- index.js
+++ index.js
@@ -3,22 +3,61 @@ let accEmail = document.querySelector('.emailField')
 let accUsername = document.querySelector('.usernameField')
 let accPassword = document.querySelector('.passwordField')
 
-let nameErr = document.getElementsByClassName('.nameErr')
+let signInName = document.querySelector('.signInUsernameField')
+let signInPassword = document.querySelector('.signInPasswordField')
 
-let submit = document.getElementById('submit')
+let signUp = document.getElementById('signUpButton')
+let signIn = document.getElementById('signInButton')
 
 let form = document.getElementById('form')
 
-document.addEventListener('submit', () => {
-    
-    let errors = []
+var users = [
+    {
+        name:'stan',
+        email:'sjgogan@me.com',
+        username:'stang123',
+        password:'apple123',
+    },
+    {
+        name:'',
+        email:'',
+        username:'',
+        password:'',
+    }
+]
 
-    if(accName.innerText === '' || accName.innerText == null){
-        errors.push('name field cannot be empty')
-        nameErr.innerText = errors
+function submitForm(){
+    const user = document.querySelector('.signInUsernameField').value
+    const pass = document.querySelector('.signInPasswordField').value
+
+    let nameErr = document.querySelector('.nameErr')
+    let passErr = document.querySelector('.passErr')
+
+    if(user === users[0].username && pass === users[0].password){
+        window.location.replace("./account.html");
+    }else{
+        console.log('nope user')
     }
 
-    if(errors.length > 0){
-        console.log('no errors')
+    if(user != users[0].username){
+        nameErr.innerText = 'invalid username'
+        console.log('wrong')
     }
+
+    if(pass != users[0].password){
+        passErr.innerText = 'invalid password'
+        console.log('wrong')
+    }
+
+    // if(user.innerText === users[0].username && signInPassword.innerText === users[0].password){
+    //     console.log('hooray')
+    // }
+
+// console.log('hello')
+}
+
+signIn.addEventListener('click', function(){
+
+    submitForm()
+
 })
